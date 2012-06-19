cb0d957350b248cc16c40c8ed31124951db8e1e1
Mike Fogel <mike@fogel.ca>
2012-06-18 18:59:22 -0700

    Restrict send/sent invite Views to logged-in Users

--- invitation/views.py
+++ invitation/views.py
@@ -1,15 +1,23 @@
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, FormView
 
 from invitation.models import InvitationKey
 from invitation.forms import InvitationKeyForm
 
 
-class SendInviteView(FormView):
+class LoginRequiredMixin(object):
+    "Restrict a View to only logged-in Users"
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+
+class SendInviteView(LoginRequiredMixin, FormView):
     "Send an invite"
     form_class = InvitationKeyForm
-    template_name='invitation/send.html'
+    template_name = 'invitation/send.html'
     success_url = reverse_lazy('invitation_sent')
 
     def get_context_data(self, **kwargs):
@@ -26,7 +34,7 @@ class SendInviteView(FormView):
         return super(SendInviteView, self).form_valid(form)
 
 
-class InviteSentView(TemplateView):
+class InviteSentView(LoginRequiredMixin, TemplateView):
     "Invite successfully sent - success page"
     template_name = 'invitation/sent.html'
 

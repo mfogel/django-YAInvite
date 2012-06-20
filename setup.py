337a0410bd6ce97c31fdbfc4afdd6a9a7cb8cf4a
Mike Fogel <mike@fogel.ca>
2012-06-20 10:12:38 -0700

    Description edits in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,8 @@ from setuptools import setup
 setup(
     name='django-YAInvite',
     version='2.0',
-    description=(
-        'A django invite system where invites allocated to and sent by a '
-        'a model of your choosing. (Can be auth.User, but could be your '
-        'projects Account model, etc.) Invites are redeemed by a new User '
-        'signing up with an valid invite key. Supports default and '
-        'invite-specific expiration periods.'
-    ),
+    description='Yet Another Invite system for Django',
+    long_description=open('README.rst').read(),
     author='Mike Fogel',
     author_email='mike@fogel.ca',
     url='https://github.com/mfogel/django-YAInvite',

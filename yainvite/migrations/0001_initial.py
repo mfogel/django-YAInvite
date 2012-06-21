840396f2ed3d798460bb905419b133b6e5f9f29c
Mike Fogel <mike@fogel.ca>
2012-06-21 16:39:57 -0700

    Removing default values from initial migration
    See http://south.aeracode.org/ticket/925 for background

--- yainvite/migrations/0001_initial.py
+++ yainvite/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('key', self.gf('django.db.models.fields.CharField')(max_length=8, db_index=True)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True)),
-            ('expires_at', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2012, 6, 26, 0, 0), null=True, blank=True)),
+            ('expires_at', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
             ('inviter', self.gf('django.db.models.fields.related.ForeignKey')(related_name='invite_sent_set', to=orm[settings.YAINVITE_INVITER_CLASS])),
             ('redeemer', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='invite_redeemed_set', null=True, to=orm['auth.User'])),
         ))
